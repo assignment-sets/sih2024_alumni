63368670cc56c7005e3ef78ca8307663e03320b0
SubhajitRuiDas <dassubhojit775@gmail.com>
2024-09-11 21:20:07 +0530

    MAINACTIVITY UPDATE

--- app/src/main/java/com/example/alumnihub/activities/SignUpScreen.java
+++ app/src/main/java/com/example/alumnihub/activities/SignUpScreen.java
@@ -14,6 +14,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.alumnihub.MainActivity;
 import com.example.alumnihub.R;
 
 public class SignUpScreen extends AppCompatActivity {
